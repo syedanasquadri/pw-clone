b50731d1d055af46610856e6ceff7578812f2d13
syedanasquadri <syedanasquadri123@gmail.com>
2025-12-28 01:14:55 +0530

    created testimonials component

---
+++ src/Components/Testimonials.tsx
@@ -0,0 +1,133 @@
+export const Testimonials = () => {
+    return <div>
+        <div className="max-w-md sm:max-w-3xl lg:max-w-6xl mx-auto p-4">
+      <div className="max-w-2xl mx-auto text-center">
+        <h2 className="text-3xl font-bold text-slate-900 !leading-tight">What our happy client say</h2>
+        <p className="text-[15px] mt-6 leading-relaxed text-slate-600">See what our happy clients have to say. They’ve shared how our templates helped them launch quickly, look professional, and grow with ease.</p>
+      </div>
+
+      <div className="columns-1 sm:columns-2 lg:columns-3 space-y-4 mt-12">
+
+        <div className="break-inside-avoid p-6 rounded-lg bg-gray-100 relative w-full">
+          <div className="flex flex-wrap items-center gap-4">
+            <img src="https://readymadeui.com/team-2.webp" className="w-14 h-14 rounded-full border-2 border-purple-500" />
+            <div>
+              <h4 className="text-slate-900 text-[15px] whitespace-nowrap font-semibold">Michael Chen</h4>
+              <p className="mt-0.5 text-xs text-slate-500 font-medium">Food Blogger</p>
+            </div>
+          </div>
+          <div className="flex space-x-1 mt-4">
+            <svg className="w-3.5 fill-purple-600" viewBox="0 0 14 13" fill="none"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+            </svg>
+            <svg className="w-3.5 fill-purple-600" viewBox="0 0 14 13" fill="none"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+            </svg>
+            <svg className="w-3.5 fill-purple-600" viewBox="0 0 14 13" fill="none"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+            </svg>
+            <svg className="w-3.5 fill-purple-600" viewBox="0 0 14 13" fill="none"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+            </svg>
+            <svg className="w-3.5 fill-[#b5bdc0]" viewBox="0 0 14 13" fill="none"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+            </svg>
+          </div>
+          <div className="mt-6">
+            <p className="text-slate-700 font-normal text-[15px] leading-relaxed">The templates are amazing. I never had to wait for support or clarification. Everything is clear, well-documented, and the delivery is impressively fast.</p>
+          </div>
+        </div>
+
+        <div className="break-inside-avoid p-6 rounded-lg bg-gray-100 relative w-full">
+          <div className="flex flex-wrap items-center gap-4">
+            <img src="https://readymadeui.com/team-2.webp" className="w-14 h-14 rounded-full border-2 border-purple-500" />
+            <div>
+              <h4 className="text-slate-900 text-[15px] whitespace-nowrap font-semibold">John Doe</h4>
+              <p className="mt-0.5 text-xs text-slate-500 font-medium">Founder of Rubik</p>
+            </div>
+          </div>
+          <div className="flex space-x-1 mt-4">
+            <svg className="w-3.5 fill-purple-600" viewBox="0 0 14 13" fill="none"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+            </svg>
+            <svg className="w-3.5 fill-purple-600" viewBox="0 0 14 13" fill="none"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+            </svg>
+            <svg className="w-3.5 fill-purple-600" viewBox="0 0 14 13" fill="none"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+            </svg>
+            <svg className="w-3.5 fill-purple-600" viewBox="0 0 14 13" fill="none"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+            </svg>
+            <svg className="w-3.5 fill-[#b5bdc0]" viewBox="0 0 14 13" fill="none"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+            </svg>
+          </div>
+          <div className="mt-6">
+            <p className="text-slate-700 font-normal text-[15px] leading-relaxed">Exploring ReadymadeUI is like a journey through modern UI design. Each section tells a visual story, and the support documentation adds an extra layer of confidence.</p>
+          </div>
+        </div>
+
+        <div className="break-inside-avoid p-6 rounded-lg bg-gray-100 relative w-full">
+          <div className="flex flex-wrap items-center gap-4">
+            <img src="https://readymadeui.com/team-6.webp" className="w-14 h-14 rounded-full border-2 border-purple-500" />
+            <div>
+              <h4 className="text-slate-900 text-[15px] whitespace-nowrap font-semibold">Michael Foster</h4>
+              <p className="mt-0.5 text-xs text-slate-500 font-medium">Food & Travel Writer</p>
+            </div>
+          </div>
+          <div className="flex space-x-1 mt-4">
+            <svg className="w-3.5 fill-purple-600" viewBox="0 0 14 13" fill="none"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+            </svg>
+            <svg className="w-3.5 fill-purple-600" viewBox="0 0 14 13" fill="none"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+            </svg>
+            <svg className="w-3.5 fill-purple-600" viewBox="0 0 14 13" fill="none"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+            </svg>
+            <svg className="w-3.5 fill-purple-600" viewBox="0 0 14 13" fill="none"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+            </svg>
+            <svg className="w-3.5 fill-purple-600" viewBox="0 0 14 13" fill="none"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+            </svg>
+          </div>
+          <div className="mt-6">
+            <p className="text-slate-700 font-normal text-[15px] leading-relaxed">A complete game-changer for web development! From the landing page to the dashboard, every layout is spot-on. ReadymadeUI’s components are clean, scalable, and production-ready.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+}

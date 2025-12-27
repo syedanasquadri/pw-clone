f7130c6dc19ce5737f11f586fb63e299510d62c8
syedanasquadri <syedanasquadri123@gmail.com>
2025-12-28 01:15:36 +0530

    updated benefits

--- src/Components/Benefits.tsx
+++ src/Components/Benefits.tsx
@@ -1,14 +1,25 @@
-const BenefitsCard = () => {
-    return <div className="border solid">
-            <div><span>Icon</span></div>
-            <div><span>Daily Live</span></div>
-            <div><span>Interactive classes</span></div>
+import type { ReactElement } from "react"
+
+interface BenefitsCardProps{
+    Icon: ReactElement | string;
+    Heading: string;
+    SubHeading: string;
+}
+
+const BenefitsCard = ({Icon,Heading,SubHeading}: BenefitsCardProps) => {
+    return <div className="border solid ">
+            <div><span>{Icon}</span></div>
+            <div className="text-md"><span>{Heading}</span></div>
+            <div className="text-sm"><span>{SubHeading}</span></div>
             <div></div>
         </div>
 }
 
 export const Benefits = () => {
-    return <div>
-        <BenefitsCard/>
+    return <div className="grid grid-cols-2 gap-4 place-content-center">
+        <div><BenefitsCard Icon={"Cam"} Heading="Daily Live " SubHeading="Interactive classes"/></div>
+        <div><BenefitsCard Icon={"Pap"} Heading="10 Million+ " SubHeading="Tests, sample papers & notes"/></div>
+        <div><BenefitsCard Icon={"Brin"} Heading="24 x 7" SubHeading="Doubt solving sessions"/></div>
+        <div><BenefitsCard Icon={"Tick"} Heading="100 +" SubHeading="Offline centres"/></div>
     </div>
 }

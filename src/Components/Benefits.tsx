2103dd65b01a633f64cf55bfabbc912ed762344d
syedanasquadri <syedanasquadri123@gmail.com>
2025-12-28 00:38:58 +0530

    Added benefits component

---
+++ src/Components/Benefits.tsx
@@ -0,0 +1,14 @@
+const BenefitsCard = () => {
+    return <div className="border solid">
+            <div><span>Icon</span></div>
+            <div><span>Daily Live</span></div>
+            <div><span>Interactive classes</span></div>
+            <div></div>
+        </div>
+}
+
+export const Benefits = () => {
+    return <div>
+        <BenefitsCard/>
+    </div>
+}

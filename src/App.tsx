8a4ccc8022f8723b2c97a936bf76cacb71511f3e
syedanasquadri <syedanasquadri123@gmail.com>
2025-12-28 01:16:06 +0530

    updated app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Footer } from "./Components/Footer"
 import { Hero } from "./Components/Hero"
 import { Navbar } from "./Components/Navbar"
 import { ProblemAndSol } from "./Components/ProblemAndSol"
+import { Testimonials } from "./Components/Testimonials"
 
 const App = () => {
   return <div>
@@ -13,6 +14,7 @@ const App = () => {
     <ProblemAndSol/>
     <BatchDetails/>
     <Benefits/>
+    <Testimonials/>
     <CTA/>
     <Footer/>
   </div>
